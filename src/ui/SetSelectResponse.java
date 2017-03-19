eea694a264c0e923f49eefe3fe7f3b0e28add772
Sahil Patel <sahpat229@gmail.com>
2017-03-18 22:21:57 -0400

    Cleaned up a lot of the code

---
+++ src/ui/SetSelectResponse.java
@@ -0,0 +1,25 @@
+package ui;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+public class SetSelectResponse implements Sendable {
+
+  public boolean is_valid;
+  
+  public SetSelectResponse(boolean is_valid)
+  {
+    this.is_valid = is_valid;
+  }
+  
+  public void send(ObjectOutputStream outputstream)
+  {
+    try {
+      outputstream.writeObject(this);
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
+  
+}
