7b5fe383d60cbc5a6b94ad45fe264370de3bd132
sahpat229 <sahpat229@gmail.com>
2017-03-31 19:02:37 -0400

    Create SignUpMessage.java

---
+++ src/message/SignUpMessage.java
@@ -0,0 +1,36 @@
+package message;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+/*
+ * LoginMessage
+ * A Client sends a login message to the server
+ * that contains the username and password that the user
+ * Is trying to login with
+ */
+
+public class SignUpMessage implements Sendable, Serializable {
+  
+  public String username;
+  public String password;
+  
+  public SignUpMessage(String username, String password)
+  {
+    this.username = username;
+    this.password = password;
+  }
+  
+  @Override
+  public void send(ObjectOutputStream outputstream)
+  {
+    try {
+      outputstream.writeObject(this);
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
+  
+}
