dc7e71f4060f2efd8e507dd52d439075d410d805
Yash <ysharma1126@gmail.com>
2017-03-31 12:17:36 -0400

    Messages Finalized

--- src/message/LogOutMessage.java
+++ src/message/LogOutMessage.java
@@ -1,5 +1,34 @@
 package message;
 
-public class LogOutMessage {
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 
+/*
+ * LoginMessage
+ * A Client sends a login message to the server
+ * that contains the username and password that the user
+ * Is trying to login with
+ */
+
+public class LogOutMessage implements Sendable, Serializable {
+  
+  public String username;
+  
+  public LogOutMessage(String username)
+  {
+    this.username = username;
+  }
+  
+  @Override
+  public void send(ObjectOutputStream outputstream)
+  {
+    try {
+      outputstream.writeObject(this);
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
+  
 }
