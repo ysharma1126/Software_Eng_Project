03e4a50a802c4ad4b54a733e430b7a2455d25497
ksheng- <ksheng7746@gmail.com>
2017-04-01 19:36:18 -0400

    added refresh button

--- src/ui/Browser.java
+++ src/ui/Browser.java
@@ -35,9 +35,11 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Popup;
 import javafx.stage.Stage;
+import message.GamesUpdateResponse;
 import message.InitialCardsResponse;
-import message.LoadRoomsMessage;
-import message.LoadRoomsResponse;
+import message.JoinRoomMessage;
+import message.JoinRoomResponse;
+import message.RefreshMessage;
 
 public class Browser extends VBox {
   
@@ -47,24 +49,23 @@ public class Browser extends VBox {
   private final ObservableList<GameData> game_data = FXCollections.observableArrayList ();
   
   private void load_available_rooms(ObjectOutputStream outToServer, ObjectInputStream inFromServer) {
-    LoadRoomsMessage msg = new LoadRoomsMessage();
+    RefreshMessage msg = new RefreshMessage(Launcher.username);
     msg.send(outToServer);
     try {
-      // Read existing rooms as ArrayList of strings
-      LoadRoomsResponse response = (LoadRoomsResponse) inFromServer.readObject();
-      Iterator<Entry<Integer, Set<Player>>> it1 = response.rooms.entrySet().iterator();
-      Iterator<Entry<Integer, Player>> it2 = response.hosts.entrySet().iterator();
+      this.game_data.clear();
+      GamesUpdateResponse response = (GamesUpdateResponse) inFromServer.readObject();
+      Iterator<Entry<Integer, Set<Player>>> it1 = response.gameusernames.entrySet().iterator();
+      Iterator<Entry<Integer, Player>> it2 = response.gamehost.entrySet().iterator();
       while (it1.hasNext() && it2.hasNext()) {
-        Map.Entry room = (Map.Entry) it1.next();
-        Map.Entry host = (Map.Entry) it1.next();
+        Map.Entry<Integer, Set<Player>> room = (Map.Entry<Integer, Set<Player>>) it1.next();
+        Map.Entry<Integer, Player> host = (Map.Entry<Integer, Player>) it2.next();
         String gid = "Game " + room.getKey();
-        String players = ((Set) room.getValue()).size() + "/10";
-        String leader = ((Player) host.getValue()).username;
-        game_data.add(new GameData(gid, players, leader));
+        String leader = host.getValue().username;
+        String players = room.getValue().size() + "/10";
+        this.game_data.add(new GameData(gid, leader, players));
         it1.remove();
         it2.remove();
       }
-      
     } catch (ClassNotFoundException e) {
       // TODO Auto-generated catch block
       e.printStackTrace();
@@ -74,148 +75,21 @@ public class Browser extends VBox {
     }
   }
   
-  public Browser(Stage primaryStage) {
-    
-    menubar = new MenuBar();
-    content = new GridPane();
-    
-    content.setGridLinesVisible(true);
-    content.setHgap(30);
-    content.setVgap(30);
-    content.setPadding(new Insets(0, 10, 0, 10));
-    
-    // Active game browser        
-    game_tbl.setEditable(false);
-    game_tbl.setPrefWidth(460);
-    game_tbl.setPrefHeight(420);
-    game_tbl.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
-    
-    TableColumn<GameData, String> col_name = new TableColumn<>("NAME");
-    col_name.setPrefWidth(160);
-    col_name.setResizable(false);
-    col_name.setCellValueFactory(
-        new PropertyValueFactory<>("Name"));
-    
-    TableColumn<GameData, String> col_owner = new TableColumn<>("OWNER");
-    col_owner.setPrefWidth(120);
-    col_owner.setResizable(false);
-    col_owner.setCellValueFactory(
-        new PropertyValueFactory<>("Owner"));
-    
-    TableColumn<GameData, String> col_players = new TableColumn<>("PLAYERS");
-    col_players.setPrefWidth(80);
-    col_players.setResizable(false);
-    col_players.setCellValueFactory(
-        new PropertyValueFactory<>("Players"));
-    
-//    /* Local testing */
-//    HashMap<Integer, Set<Player>> rooms = new HashMap<Integer, Set<Player>>();
-//    HashMap<Integer, Player> hosts = new HashMap<Integer, Player>();
-//    
-//    Set<Player> testset = new HashSet<Player>();
-//    Player user1 = new Player("User 1");
-//    Player user2 = new Player("User 2");
-//    Player user3 = new Player("User 3");
-//    
-//    testset.add(user1);
-//    testset.add(user2);
-//    testset.add(user3);
-//    rooms.put(1, testset);
-//    rooms.put(4, testset);
-//    rooms.put(2, testset);
-//    rooms.put(3, testset);
-//    rooms.put(5, testset);
-//    
-//    hosts.put(1, user1);
-//    hosts.put(4, user2);
-//    hosts.put(2, user3);
-//    hosts.put(3, user2);
-//    hosts.put(5, user1);
-//    
-//    Iterator<Entry<Integer, Set<Player>>> it1 = rooms.entrySet().iterator();
-//    Iterator<Entry<Integer, Player>> it2 = hosts.entrySet().iterator();
-//    while (it1.hasNext() && it2.hasNext()) {
-//      Map.Entry<Integer, Set<Player>> room = (Map.Entry<Integer, Set<Player>>) it1.next();
-//      Map.Entry<Integer, Player> host = (Map.Entry<Integer, Player>) it2.next();
-//      String gid = "Game " + room.getKey();
-//      String leader = host.getValue().username;
-//      String players = room.getValue().size() + "/10";
-//      this.game_data.add(new GameData(gid, leader, players));
-//      it1.remove();
-//      it2.remove();
-//    }
-    
-    game_tbl.setItems(this.game_data);
-    game_tbl.getColumns().addAll(col_name, col_owner, col_players);
-    
-    // Disable user reordering of columns at runtime
-    game_tbl.widthProperty().addListener(new ChangeListener<Number>()
-    {
-        @Override
-        public void changed(ObservableValue<? extends Number> source, Number oldWidth, Number newWidth)
-        {
-            TableHeaderRow header = (TableHeaderRow) game_tbl.lookup("TableHeaderRow");
-            header.reorderingProperty().addListener(new ChangeListener<Boolean>() {
-                @Override
-                public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
-                    header.setReordering(false);
-                }
-            });
-        }
-    });
-    
-    game_tbl.getStyleClass().add("tbl-game");
-    
-    content.add(game_tbl, 0, 0, 1, 2);
-    
-    final Popup popup = new Popup(); 
-    popup.setX(1000); 
-    popup.setY(1000);
-    popup.getContent().addAll(new Label("ASDASDASD"));
-    // Create game button
-    Button newgame_btn = new Button("NEW GAME");
-    newgame_btn.getStyleClass().add("btn-newgame");
-    newgame_btn.setOnAction(new EventHandler<ActionEvent>() {
-      @Override
-      public void handle(ActionEvent event) {
-        popup.show(primaryStage);
-      }
-    });
-    
-    
-    content.add(newgame_btn, 0, 2, 1, 1);
-    
-    // User info
-    GridPane userinfo = new GridPane();
-    userinfo.setGridLinesVisible(true);
-    userinfo.setPrefWidth(230);
-    userinfo.setPrefHeight(120);
-    userinfo.setHgap(10);
-    userinfo.setVgap(10);
-    userinfo.setPadding(new Insets(0, 10, 0, 10));
-    
-    ImageView user_avatar = new ImageView();
-    Label user_name = new Label(Launcher.username);
-    HBox user_wins = new HBox();
-    
-    Label ngold = new Label("4");
-    Label nsilver = new Label("2");
-    Label nbronze = new Label("0");
-    
-    user_wins.getChildren().addAll(ngold, nsilver, nbronze);
-    userinfo.add(user_avatar, 0, 0, 1, 1);
-    userinfo.add(user_name, 1, 0, 1, 1);
-    userinfo.add(user_wins, 0, 1, 2, 1);
-    userinfo.getStyleClass().add("user");
-    content.add(userinfo, 1, 0, 1, 1);
-        
-    // Online Users / chat
-    
-    this.getChildren().addAll(menubar, content);
-    this.getStyleClass().add("browser");
-    this.setPadding(new Insets(0, 40, 0, 40));
+  private void join_room(Stage primaryStage, ObjectOutputStream outToServer, ObjectInputStream inFromServer, Integer gid) {
+    JoinRoomMessage msg = new JoinRoomMessage(Launcher.username, gid);
+    msg.send(outToServer);
+    try {
+      JoinRoomResponse response = (JoinRoomResponse) inFromServer.readObject();
+      // Launcher.openGame(Stage primaryStage);
+    } catch (ClassNotFoundException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
   }
-  
+
   public Browser(Stage primaryStage, ObjectOutputStream outToServer, ObjectInputStream inFromServer) {
     
     menubar = new MenuBar();
@@ -250,17 +124,17 @@ public class Browser extends VBox {
     col_players.setCellValueFactory(
         new PropertyValueFactory<>("Players"));
     
-    Thread server_response_handler = null;
-    try {
-      server_response_handler = new Thread(new BrowserThread(primaryStage, outToServer, inFromServer));
-    } catch (IOException e2) {
-      // TODO Auto-generated catch block
-      e2.printStackTrace();
-    }
-    server_response_handler.start();
+//    Thread server_response_handler = null;
+//    try {
+//      server_response_handler = new Thread(new BrowserThread(primaryStage, outToServer, inFromServer));
+//    } catch (IOException e2) {
+//      // TODO Auto-generated catch block
+//      e2.printStackTrace();
+//    }
+//    server_response_handler.start();
     
-    load_available_rooms(outToServer, inFromServer);
-    game_tbl.setItems(game_data);
+    //load_available_rooms(outToServer, inFromServer);
+    game_tbl.setItems(this.game_data);
     game_tbl.getColumns().addAll(col_name, col_owner, col_players);
     
     // Disable user reordering of columns at runtime
@@ -281,12 +155,23 @@ public class Browser extends VBox {
     
     game_tbl.getStyleClass().add("tbl-game");
     
-    content.add(game_tbl, 0, 0, 1, 2);
+    content.add(game_tbl, 0, 0, 3, 2);
     
     // Create game button
     Button newgame_btn = new Button("NEW GAME");
     newgame_btn.getStyleClass().add("btn-newgame");
     content.add(newgame_btn, 0, 2, 1, 1);
+    
+    // Create refresh button
+    Button refresh_btn = new Button("*");
+    refresh_btn.getStyleClass().add("btn-refresh");
+    content.add(refresh_btn, 2, 2, 1, 1);
+    refresh_btn.setOnAction(new EventHandler<ActionEvent>(){
+      @Override
+      public void handle(ActionEvent e) {
+        load_available_rooms(outToServer, inFromServer);
+      }
+    });
     
     // User info
     GridPane userinfo = new GridPane();
@@ -310,11 +195,11 @@ public class Browser extends VBox {
     userinfo.add(user_name, 1, 0, 1, 1);
 //    userinfo.add(user_wins, 0, 1, 2, 1);
     userinfo.getStyleClass().add("user");
-    content.add(userinfo, 1, 0, 1, 1);
+    content.add(userinfo, 3, 0, 1, 1);
         
     // Online Users / chat
     
-    this.getChildren().addAll(menubar, content);
+    this.getChildren().addAll(content);
     this.getStyleClass().add("browser");
     this.setPadding(new Insets(0, 40, 0, 40));
   }
--- src/ui/Launcher.java
+++ src/ui/Launcher.java
@@ -39,14 +39,26 @@ public class Launcher extends Application {
     primaryStage.show();
   }
   
-  public static void openBrowser(Stage primaryStage)
-  {
-    
-  }
+//  public static void openBrowser(Stage primaryStage)
+//  {
+//    Browser browser = new Browser(primaryStage);
+//    Scene scene = new  Scene(browser, 800, 600);
+//    scene.getStylesheets().add("ui/style.css"); 
+//    primaryStage.setTitle("SET");
+//    primaryStage.setResizable(false);
+//    primaryStage.setScene(scene);
+//    primaryStage.show();
+//  }
   
   public static void openBrowser(Stage primaryStage, ObjectOutputStream outToServer, ObjectInputStream inFromServer)
   {
-    System.out.println("Logged in!");
+    Browser browser = new Browser(primaryStage, outToServer, inFromServer);
+    Scene scene = new  Scene(browser, 800, 600);
+    scene.getStylesheets().add("ui/style.css"); 
+    primaryStage.setTitle("SET");
+    primaryStage.setResizable(false);
+    primaryStage.setScene(scene);
+    primaryStage.show();
   }
   
   public static void openGame(Stage primaryStage, ArrayList<String> users)
@@ -68,27 +80,26 @@ public class Launcher extends Application {
   @Override
   public void start(Stage primaryStage)
   {
-    openLogin(primaryStage);
-//    String hostname = "199.98.20.114";
-//    int portnumber = 8080;
-//    
-//    try {
-//      Socket connectSocket = new Socket(hostname, portnumber);
-//      ObjectOutputStream outToServer = new ObjectOutputStream(connectSocket.getOutputStream());
-//      ObjectInputStream inFromServer = new ObjectInputStream(connectSocket.getInputStream());
-//      System.out.println("Opening Login");
-//      openLogin(primaryStage, outToServer, inFromServer);       
-//    }
-//    
-//    catch (ConnectException e1) {
-//      System.err.println("Connect Did not enter in proper server address/portnumber!");
-//    } catch (UnknownHostException e1) {
-//    // TODO Auto-generated catch block
-//      System.err.println("Did not enter in proper server address/portnumber!");
-//    } catch (IOException e1) {
-//      // TODO Auto-generated catch block
-//          e1.printStackTrace();
-//    }
+    String hostname = "199.98.20.114";
+    int portnumber = 8080;
+    
+    try {
+      Socket connectSocket = new Socket(hostname, portnumber);
+      ObjectOutputStream outToServer = new ObjectOutputStream(connectSocket.getOutputStream());
+      ObjectInputStream inFromServer = new ObjectInputStream(connectSocket.getInputStream());
+      System.out.println("Opening Login");
+      openLogin(primaryStage, outToServer, inFromServer);       
+    }
+    
+    catch (ConnectException e1) {
+      System.err.println("Connect Did not enter in proper server address/portnumber!");
+    } catch (UnknownHostException e1) {
+    // TODO Auto-generated catch block
+      System.err.println("Did not enter in proper server address/portnumber!");
+    } catch (IOException e1) {
+      // TODO Auto-generated catch block
+          e1.printStackTrace();
+    }
   }
 }
 
